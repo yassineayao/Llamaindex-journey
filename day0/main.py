b17ec28e1d58751b11b97ac000460a7f282a418f
Yassine Ayaou <yassineayao@gmail.com>
2024-03-15 16:21:31 +0000

    Add string documentation

--- day0/main.py
+++ day0/main.py
@@ -10,6 +10,15 @@ from llama_index.core.query_engine import BaseQueryEngine
 
 
 class RAG:
+    """
+    Retrieval Augmented Generation (RAG).
+
+    Attributes:
+        DATA_FOLDER_PATH (str): The path to the data folder
+        PERSIST_DIR (str): The directory for persisting indexes.
+        query_engine (BaseQueryEngine): The query engine for performing searches.
+    """
+
     DATA_FOLDER_PATH: str
     PERSIST_DIR: str
     query_engine: BaseQueryEngine
@@ -19,17 +28,27 @@ class RAG:
         data_folder_path: str = os.path.join(".", "day0", "data"),
         persist_dir: str = os.path.join(".", "day0", "storage"),
     ) -> None:
+        """
+        Initializes the RAG with specified parameters.
+
+        Args:
+            data_folder_path (str, optional): The path to the data folder. Defaults to "./day0/data".
+            persist_dir (str, optional): The directory for persisting indexes. Defaults to "./day0/storage".
+        """
         # set base paths
         self.DATA_FOLDER_PATH = data_folder_path
         self.PERSIST_DIR = persist_dir
 
-        # Create/load indeces
+        # Create/load indexes
         if not os.path.exists(self.PERSIST_DIR):
             self.create_index()
         else:
             self.load_existing_index()
 
     def create_index(self):
+        """
+        Creates a new index from the data and stores it.
+        """
         # Loading data
         documents = SimpleDirectoryReader(self.DATA_FOLDER_PATH).load_data()
 
@@ -41,11 +60,23 @@ class RAG:
         self.query_engine = index.as_query_engine()
 
     def load_existing_index(self):
+        """
+        Loads an existing index from storage.
+        """
         storage_context = StorageContext.from_defaults(persist_dir=self.PERSIST_DIR)
         index = load_index_from_storage(storage_context)
         self.query_engine = index.as_query_engine()
 
     def run(self, query):
+        """
+        Executes a query using the query engine and returns the results.
+
+        Args:
+            query: The query to be executed.
+
+        Returns:
+            QueryResult: The LLM response.
+        """
         return self.query_engine.query(query)
 
 
